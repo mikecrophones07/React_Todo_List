bd582e5b13f82078ccbb617a0b95831a1b8c6859
Mike Anthony A. Echon <mike.toradora07@gmail.com>
2019-10-22 08:18:50 +0800

    initial commit

---
+++ react-todo-list/src/components/ViewGroup/ViewGroup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CounterGroup extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <p>AA</p>
+            </div>
+        )
+    }
+}
+
+export default CounterGroup;
